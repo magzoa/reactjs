0b2423c025be32c40525d12055f9eb7b7c844f33
magzoaa <magzoaa@gmail.com>
2019-05-07 12:33:31 -0400

    Codigo inicial de la aplicación en REACT

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h2>Bienvenido a REAC 3</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
