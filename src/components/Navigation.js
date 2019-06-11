3a50e18d67146f586ab60233e4f5690e0749b805
magzoaa <magzoaa@gmail.com>
2019-06-10 21:40:27 -0400

    Video a los 32 y 21 minutos react

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React,{Component} from  'react';
+
+class  Navigation extends Component {
+  render() {
+
+      return(
+        <nav className="navbar navbar-dark bg-dark">
+
+        <a href="" className="text-write">
+      {this.props.titulo}
+        </a>
+        </nav>
+      )
+  }
+}
+
+export default Navigation;
